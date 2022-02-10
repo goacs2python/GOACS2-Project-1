c51477bb532adde6e6bd04d4a26a2ba927d94405
Margaret Kim <mkim@asij.ac.jp>
2022-02-10 12:52:12 +0900

    update groupstrategies.py

--- groupstrategies.py
+++ groupstrategies.py
@@ -1,16 +1,16 @@
 # YOU ONLY HAVE TO SUBMIT FUNCTIONS FOR WHICH
 # YOU ARE THE DRIVER IN PAIR PROGRAMMING
 
-# Your team's 1st strategy
+# Your team's 1st strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
 # 
 
 
-# Your team's 2nd strategy
+# Your team's 2nd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
 # 
 
 
-# Your team's 3rd strategy
+# Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
 # 
