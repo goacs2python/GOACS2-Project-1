81d5e51e2cac0181b139992e93ba44f6707889b4
mkimgoa <75723619+mkimgoa@users.noreply.github.com>
2022-02-24 09:21:00 +0900

    Update individualstrategies.py

--- individualstrategies.py
+++ individualstrategies.py
@@ -1,4 +1,9 @@
-# strategy1()
+
+# Here are some history variables to test your code on. Feel free to create your own.
+hist1 = []
+hist2 = [("split","steal")]
+hist3 = [("split","split"),("steal","split"),("split,steal"),("split,split"),("steal,split")]
+hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split"),("split","split"),("steal","split")]# strategy1()
 
 
 # strategy2()
